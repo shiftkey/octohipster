92f1208e313bb64a8966a3b439e07978ea94b0d0
Brendan Forster <brendan@github.com>
2014-05-25 16:38:52 -0700

    rewrite old bindings as RxUI bindings

--- OctoHipster/Views/ShellView.xaml.cs
+++ OctoHipster/Views/ShellView.xaml.cs
@@ -1,10 +1,31 @@
-﻿namespace OctoHipster.Views
+﻿using System.Windows;
+using OctoHipster.ViewModels;
+using ReactiveUI;
+
+namespace OctoHipster.Views
 {
-    public partial class ShellView
+    public partial class ShellView : IViewFor<IShellViewModel>
     {
         public ShellView()
         {
             InitializeComponent();
+
+            this.Bind(ViewModel, vm => vm.SearchText, v => v.SearchText.Text);
+        }
+
+        object IViewFor.ViewModel
+        {
+            get { return GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register(
+            "ViewModel", typeof (IShellViewModel), typeof (ShellView), new PropertyMetadata(default(IShellViewModel)));
+
+        public IShellViewModel ViewModel
+        {
+            get { return (IShellViewModel) GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
         }
     }
 }
