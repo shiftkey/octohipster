fe3427cfe1d9ed080be79fb89df491a8c353ff6c
Brendan Forster <brendan@github.com>
2014-06-02 10:37:02 +0200

    fixup! binding :poop:

--- OctoHipster/Views/ShellView.xaml.cs
+++ OctoHipster/Views/ShellView.xaml.cs
@@ -10,22 +10,29 @@ namespace OctoHipster.Views
         {
             InitializeComponent();
 
+            // workaround until we get all this looking :gem:
+            DataContextChanged += (s, e) => { ViewModel = DataContext as IShellViewModel; };
+
             this.Bind(ViewModel, vm => vm.SearchText, v => v.SearchText.Text);
         }
 
         object IViewFor.ViewModel
         {
-            get { return GetValue(ViewModelProperty); }
-            set { SetValue(ViewModelProperty, value); }
+            get { return ViewModel; }
+            set { ViewModel = value as IShellViewModel; }
         }
-
-        public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register(
-            "ViewModel", typeof (IShellViewModel), typeof (ShellView), new PropertyMetadata(default(IShellViewModel)));
 
         public IShellViewModel ViewModel
         {
-            get { return (IShellViewModel) GetValue(ViewModelProperty); }
+            get { return GetValue(ViewModelProperty) as IShellViewModel; }
             set { SetValue(ViewModelProperty, value); }
         }
+
+        public static readonly DependencyProperty ViewModelProperty =
+           DependencyProperty.Register(
+               "ViewModel",
+               typeof(IShellViewModel),
+               typeof(ShellView),
+               new PropertyMetadata(null));
     }
 }
